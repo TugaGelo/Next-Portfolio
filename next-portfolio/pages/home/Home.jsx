5aaed907101f0821df2508eb7cf098f43f00fa67
Angelo-HiPE <roland_a_tugaoen@hipe.asia>
2026-01-29 10:32:05 +0800

    fix: folder structure

--- next-portfolio/pages/home/Home.jsx
+++ next-portfolio/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './home.module.css';
-import { Socials } from './Socials';
-import Data from './Data';
+import { Socials } from '../../components/Socials';
+import Data from '../../components/Data';
 import ScrollDown from './ScrollDown';
 
 const Home = () => {
